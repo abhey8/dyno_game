57455712088f3750a0dc9a1bb3f43b592cf0b6e4
ABHEY DUA <abheydua2025@ABHEYs-MacBook-Pro.local>
2025-03-29 21:31:03 +0530

    first js game

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+import DinoGame from './DinoGame';
+import Style from './App.css';
+
+
+function App(){
+  <>
+    <Style />
+    <DinoGame />
+  </>
+};
+
+export default App;
